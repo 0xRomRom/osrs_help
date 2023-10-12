c6c36c37acc288a42fa2b51228e7848e701f85de
Roman - <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>
2023-10-12 20:18:07 +0200

    Logo added

--- osrs_help/src/components/nav/Nav.jsx
+++ osrs_help/src/components/nav/Nav.jsx
@@ -1,7 +1,12 @@
 import stl from "./Nav.module.css";
+import logo from "../../assets/OSRSHELP.png";
 
 const Nav = () => {
-  return <div className={stl.nav}></div>;
+  return (
+    <div className={stl.nav}>
+      <img src={logo} alt="OSRS Help logo" className={stl.logo} />
+    </div>
+  );
 };
 
 export default Nav;
