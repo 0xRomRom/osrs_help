39af916e8fa4da4692f260afabb58385c87358fb
Roman - <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>
2023-10-12 23:19:10 +0200

    Home screen additions

--- osrs_help/src/components/main/home/Home.jsx
+++ osrs_help/src/components/main/home/Home.jsx
@@ -1,6 +1,7 @@
 import stl from "./Home.module.css";
 import barrows from "../../../assets/wallpapers/Barrows.webp";
 import welcometxt from "../../../assets/Welcometxt.png";
+import login from "../../../assets/wallpapers/Login.webp";
 
 const Home = () => {
   return (
@@ -9,6 +10,15 @@ const Home = () => {
         <img src={barrows} alt="Banner Image" className={stl.bannerimage} />
       </div>
       <img src={welcometxt} alt="Welcome Text" className={stl.welcometxt} />
+      <div className={stl.modalbottom}>
+        <p className={stl.introtext}>
+          A central place for Oldschool Runescape tools.
+        </p>
+        <div className={stl.userbox}>
+          <span className={stl.getinfo}>Get Account Info</span>
+          <img src={login} alt="Loginscreen" className={stl.loginbg} />
+        </div>
+      </div>
     </div>
   );
 };
