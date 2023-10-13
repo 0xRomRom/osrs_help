bfc0c8ee405af22b8b7bdb335cf928684a528749
Roman - <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>
2023-10-13 10:09:20 +0200

    Ugly ass modal added

--- osrs_help/src/components/main/home/Home.jsx
+++ osrs_help/src/components/main/home/Home.jsx
@@ -1,13 +1,12 @@
 import stl from "./Home.module.css";
 import barrows from "../../../assets/wallpapers/Barrows.webp";
 import welcometxt from "../../../assets/Welcometxt.png";
-import login from "../../../assets/wallpapers/Login.webp";
 
 const Home = () => {
   return (
     <div className={stl.modal}>
       <div className={stl.imageBox}>
-        <img src={barrows} alt="Banner Image" className={stl.bannerimage} />
+        <img src={barrows} alt="Banner" className={stl.bannerimage} />
       </div>
       <img src={welcometxt} alt="Welcome Text" className={stl.welcometxt} />
       <div className={stl.modalbottom}>
@@ -15,8 +14,11 @@ const Home = () => {
           A central place for Oldschool Runescape tools.
         </p>
         <div className={stl.userbox}>
-          <span className={stl.getinfo}>Get Account Info</span>
-          <img src={login} alt="Loginscreen" className={stl.loginbg} />
+          <div>
+            <label>Username:</label>
+            <input type="text" className={stl.nameinput} />
+          </div>
+          <button className={stl.cta}>Get stats</button>
         </div>
       </div>
     </div>
