5d864d0c03a75aa3c3dbe8ec63b282748f7cfc91
Roman - <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>
2023-10-22 01:21:45 +0200

    Gradients added for all skills

--- src/components/main/skills/SkillsTab.jsx
+++ src/components/main/skills/SkillsTab.jsx
@@ -29,19 +29,19 @@ const SkillsTab = () => {
   return (
     <div className={stl.modal}>
       <div className={stl.modalInner}>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.attack}`}>
           <img src={attackIcon} alt="Attack Icon" className={stl.iconImg} />
           <span className={stl.skillName}>Attack</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.defence}`}>
           <img src={defenceIcon} alt="Attack Icon" className={stl.iconImg} />
           <span className={stl.skillName}>Defence</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.strength}`}>
           <img src={strengthIcon} alt="Strength Icon" className={stl.iconImg} />
           <span className={stl.skillName}>Strength</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.hitpoints}`}>
           <img
             src={hitpointsIcon}
             alt="Hitpoints Icon"
@@ -49,23 +49,23 @@ const SkillsTab = () => {
           />
           <span className={stl.skillName}>Hitpoints</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.ranged}`}>
           <img src={rangedIcon} alt="Ranged Icon" className={stl.iconImg} />
           <span className={stl.skillName}>Ranged</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.prayer}`}>
           <img src={prayerIcon} alt="Prayer Icon" className={stl.iconImg} />
-          <span className={stl.skillName}>Prayer</span>
+          <span className={`${stl.skillName} ${stl.black}`}>Prayer</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.magic}`}>
           <img src={magicIcon} alt="Magic Icon" className={stl.iconImg} />
           <span className={stl.skillName}>Magic</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.cooking}`}>
           <img src={cookingIcon} alt="Cooking Icon" className={stl.iconImg} />
           <span className={stl.skillName}>Cooking</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.woodcutting}`}>
           <img
             src={woodcuttingIcon}
             alt="Woodcutting Icon"
@@ -73,7 +73,7 @@ const SkillsTab = () => {
           />
           <span className={stl.skillName}>Woodcutting</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.fletching}`}>
           <img
             src={fletchingIcon}
             alt="Fletching Icon"
@@ -81,11 +81,11 @@ const SkillsTab = () => {
           />
           <span className={stl.skillName}>Fletching</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.fishing}`}>
           <img src={fishingIcon} alt="Fishing Icon" className={stl.iconImg} />
           <span className={stl.skillName}>Fishing</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.firemaking}`}>
           <img
             src={firemakingIcon}
             alt="Firemaking Icon"
@@ -93,39 +93,39 @@ const SkillsTab = () => {
           />
           <span className={stl.skillName}>Firemaking</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.crafting}`}>
           <img src={craftingIcon} alt="Crafting Icon" className={stl.iconImg} />
           <span className={stl.skillName}>Crafting</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.smithing}`}>
           <img src={smithingIcon} alt="Smithing Icon" className={stl.iconImg} />
           <span className={stl.skillName}>Smithing</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.mining}`}>
           <img src={miningIcon} alt="Mining Icon" className={stl.iconImg} />
           <span className={stl.skillName}>Mining</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.herblore}`}>
           <img src={herbloreIcon} alt="Herblore Icon" className={stl.iconImg} />
           <span className={stl.skillName}>Herblore</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.agility}`}>
           <img src={agilityIcon} alt="Agility Icon" className={stl.iconImg} />
           <span className={stl.skillName}>Agility</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.thieving}`}>
           <img src={thievingIcon} alt="Thieving Icon" className={stl.iconImg} />
           <span className={stl.skillName}>Thieving</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.slayer}`}>
           <img src={slayerIcon} alt="Slayer Icon" className={stl.iconImg} />
           <span className={stl.skillName}>Slayer</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.farming}`}>
           <img src={farmingIcon} alt="Farming Icon" className={stl.iconImg} />
           <span className={stl.skillName}>Farming</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.runecrafting}`}>
           <img
             src={runecraftingIcon}
             alt="Runecrafting Icon"
@@ -133,11 +133,11 @@ const SkillsTab = () => {
           />
           <span className={stl.skillName}>Runecraft</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.hunter}`}>
           <img src={hunterIcon} alt="Hunter Icon" className={stl.iconImg} />
           <span className={stl.skillName}>Hunter</span>
         </div>
-        <div className={stl.skill}>
+        <div className={`${stl.skill} ${stl.construction}`}>
           <img
             src={constructionIcon}
             alt="Construction Icon"
