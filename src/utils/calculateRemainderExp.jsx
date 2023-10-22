01e984e54c3f331aceb6035363f80be7233eaa10
Roman - <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>
2023-10-22 04:45:17 +0200

    Helper component cleaned up

--- src/utils/calculateRemainderExp.jsx
+++ src/utils/calculateRemainderExp.jsx
@@ -3,10 +3,9 @@ import { osrsXpTable } from "./playerStats";
 const CalculateRemainderExp = (props) => {
   const calculateExpUntilNextLevel = () => {
     const skill = props.skillname;
+
     const currentLvl = +props.currentLvl;
     const currentExp = +props.currentExp[skill];
-    const levelStartExp = osrsXpTable[currentLvl];
-
     const nextLevelStartExp = osrsXpTable[currentLvl + 1];
 
     const remainder = (nextLevelStartExp - currentExp).toLocaleString();
