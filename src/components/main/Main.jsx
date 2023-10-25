25198ad9c3fe642e6b8cd404e2bb9ae014e913c2
Roman - <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>
2023-10-25 03:54:05 +0200

    Rebuild

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -5,26 +5,29 @@ import SkillsTab from "./skills/SkillsTab";
 const Main = (props) => {
   return (
     <div className={stl.main}>
-      {props.activeTab === "home" && (
-        <Home
-          setSkills={props.setSkills}
-          skills={props.skills}
-          setSkillsExp={props.setSkillsExp}
-          skillsExp={props.skillsExp}
-          setPlayerName={props.setPlayerName}
-          playerName={props.playerName}
-        />
-      )}
-      {props.activeTab === "skills" && (
-        <SkillsTab
-          setSkills={props.setSkills}
-          skills={props.skills}
-          setSkillsExp={props.setSkillsExp}
-          skillsExp={props.skillsExp}
-          setPlayerName={props.setPlayerName}
-          playerName={props.playerName}
-        />
-      )}
+      <div className={stl.adBar}>[ Advertisements ]</div>
+      <div className={stl.content}>
+        {props.activeTab === "home" && (
+          <Home
+            setSkills={props.setSkills}
+            skills={props.skills}
+            setSkillsExp={props.setSkillsExp}
+            skillsExp={props.skillsExp}
+            setPlayerName={props.setPlayerName}
+            playerName={props.playerName}
+          />
+        )}
+        {props.activeTab === "skills" && (
+          <SkillsTab
+            setSkills={props.setSkills}
+            skills={props.skills}
+            setSkillsExp={props.setSkillsExp}
+            skillsExp={props.skillsExp}
+            setPlayerName={props.setPlayerName}
+            playerName={props.playerName}
+          />
+        )}
+      </div>
     </div>
   );
 };
