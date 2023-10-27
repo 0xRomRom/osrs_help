12bce2c11ff6ad6a30fe4db25aa1f94597aaf2f6
Roman - <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>
2023-10-28 01:20:56 +0200

    Monster list formatted

--- src/components/main/skills/programs/attack/npcgrid/NPCGrid.jsx
+++ src/components/main/skills/programs/attack/npcgrid/NPCGrid.jsx
@@ -1,21 +1,39 @@
 import stl from "./NPCGrid.module.css";
 import monsterList from "../../../../../../utils/monsterList";
+import attackLogo from "../../../../../../assets/skillicons/Attack.webp";
+import healthLogo from "../../../../../../assets/skillicons/Hitpoints.webp";
+import slayerLogo from "../../../../../../assets/skillicons/Slayer.png";
+import memberLogo from "../../../../../../assets/icons/Member.webp";
 
 const NPCGrid = () => {
   return (
     <div className={stl.grid}>
       <div className={stl.typeRow}>
-        <span>Monster</span>
-        <span>Member</span>
-        <span>Hitpoints</span>
-        <span>Kill</span>
+        <span className={stl.monsterTitleRow}>
+          <img src={attackLogo} alt="Attack Logo" className={stl.miniLogo} />{" "}
+          Monster
+        </span>
+        <span>
+          <img src={memberLogo} alt="Member Logo" className={stl.miniLogo} />{" "}
+          Member
+        </span>
+        <span>
+          <img src={healthLogo} alt="Health Logo" className={stl.miniLogo} />{" "}
+          Hitpoints
+        </span>
+        <span>
+          <img src={slayerLogo} alt="Slayer Logo" className={stl.miniLogo} /> To
+          Go
+        </span>
       </div>
       <div className={stl.resultGrid}>
         {monsterList.map((monster) => {
           return (
             <>
               <div className={stl.row}>
-                <span className={stl.rowItem}>{monster.monster}</span>
+                <span className={`${stl.rowItem} ${stl.monsterRow}`}>
+                  {monster.monster}
+                </span>
                 <span className={stl.rowItem}>
                   {monster.member ? "Yes" : "No"}
                 </span>
